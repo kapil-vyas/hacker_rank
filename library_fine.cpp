6aaaa9741829f48e9b1b42961f15e664ec4d601d
Kapil Vyas <kapil.akshar@gmail.com>
2015-06-25 23:54:22 -0500

    Fine Details and Quick Algo

--- library_fine.cpp
+++ library_fine.cpp
@@ -11,11 +11,20 @@ typedef struct date {
     int year;
 } Date;
 
+// Fine details:
+// D M Y
+// x y z actual
+// a b c expected
+// on or before expected: act_date <= exp_date return 0
+// same month: z <= c and y == b return 15*(x-a)
+// same year: z == c return 500*(y-b)
+// else: return 10000
+
 int main() {
     Date exp_date; // expected date
     Date act_date; // actual date
-    cin << exp_date.day << exp_date.month << exp_date.year;
     cin << act_date.date << act_date.month << act_date.year;
+    cin << exp_date.day << exp_date.month << exp_date.year;
     return 0;
 }
 
