1729207822855a218deeaf38615fd04f7e96d307
Kapil Vyas <kapil.akshar@gmail.com>
2015-06-26 23:28:13 -0500

    Add Tests Cases - TDD approach

--- library_fine.cpp
+++ library_fine.cpp
@@ -25,6 +25,26 @@ int main() {
     Date act_date; // actual date
     cin << act_date.date << act_date.month << act_date.year;
     cin << exp_date.day << exp_date.month << exp_date.year;
+
+    // Test 1
+    // DD MM YYYY
+    // 01 01 2015 exp_date
+    // 01 01 2015 same date (act date)
+    if(getFine(exp_date, act_date) == 0)
+      cout << "Test 1 pass" << endl;
+    // 12 25 2014 before date (act date)
+    if(getFine(exp_date, act_date) == 0)
+      cout << "Test 2 pass" << endl;
+    // 14 01 2015 same month
+    if(getFine(exp_date, act_date) == 15*(14-1))
+      cout << "Test 3 pass" << endl;
+    // 01 02 2015 same year
+    if(getFine(exp_date, act_date) == 500*(2-1))
+      cout << "Test 4 pass" << endl;
     return 0;
+}
+
+int getFine(Date exp_date, Date act_date) {
+  return 0;
 }
 
