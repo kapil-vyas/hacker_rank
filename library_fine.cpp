90d8b01d8050f7d6eff8d4afac49f8d067919fd6
Kapil Vyas <kapil.akshar@gmail.com>
2015-06-27 23:40:56 -0500

    Remove comments describing algo

--- library_fine.cpp
+++ library_fine.cpp
@@ -11,15 +11,6 @@ typedef struct date {
     int year;
 } Date;
 
-// Fine details:
-// D M Y
-// x y z actual
-// a b c expected
-// on or before expected: act_date <= exp_date return 0
-// same month: z <= c and y == b return 15*(x-a)
-// same year: z == c return 500*(y-b)
-// else: return 10000
-
 int main() {
     Date exp_date; // expected date
     Date act_date; // actual date
@@ -41,7 +32,7 @@ int main() {
     // 01 02 2015 same year
     if(getFine(exp_date, act_date) == 500*(2-1))
       cout << "Test 4 pass" << endl;
-    return 0;
+    return 10000
 }
 
 int getFine(Date exp_date, Date act_date) {
